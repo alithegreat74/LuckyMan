05bbe3e72d80352c47f0bc674b5f70824b652d78
Alireza Mohammadi <86660759+alithegreat74@users.noreply.github.com>
2025-01-07 14:34:07 +0330

    Update NetworkManager.cs

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -15,51 +15,6 @@ namespace Network
         private Model.NetworkModelInfo _networkModel;
         private SmartFox _smartFox;
 
-        public async Task<BaseEvent> SendRequest(IRequest request, string sfsEvent)
-        {
-            var taskCompletionSource = new TaskCompletionSource<BaseEvent>();
-            if (_smartFox == null)
-            {
-                taskCompletionSource.SetException(new Exception("Smartfox is not initialized"));
-                return await taskCompletionSource.Task;
-            }
-            // creating a lambda that sets the result
-            // we need to Unsubscribe from it in order to not raise the thing
-            EventListenerDelegate lambda = null;
-            lambda = (BaseEvent e) =>
-            {
-                taskCompletionSource.SetResult(e);
-                _smartFox.RemoveEventListener(sfsEvent, lambda);
-            };
-            _smartFox.AddEventListener(sfsEvent, lambda);
-            _smartFox.Send(request);
-            // What happens when your request times out
-            var timeoutTask = Task.Delay(_networkModel.Timeout)
-                .ContinueWith(t =>
-                {
-                    taskCompletionSource.TrySetException(new Exception("Request Timed Out"));
-                    _smartFox.RemoveEventListener(sfsEvent, lambda);
-                });
-
-            return
-                await Task.WhenAny(timeoutTask, taskCompletionSource.Task)
-                == taskCompletionSource.Task
-                ? await taskCompletionSource.Task
-                : throw new TimeoutException("Request Timed Out");
-        }
-
-        public void SubscribeToEvent(NetworkEventSubscription subscription) =>
-            _smartFox.AddEventListener(subscription.EventName, subscription.Action);
-
-        public void UnSubscribeToEvent(NetworkEventSubscription subscription) =>
-            _smartFox.RemoveEventListener(subscription.EventName, subscription.Action);
-
-        public User GetMyself() => _smartFox.MySelf;
-
-        public Room GetCurrentRoom() => _smartFox.LastJoinedRoom;
-
-        public string GetCurrentZone() => _smartFox.CurrentZone;
-
         private void Awake()
         {
             _smartFox = new SmartFox();
@@ -76,19 +31,102 @@ namespace Network
         {
             if (_smartFox == null || !_smartFox.IsConnected)
                 return;
+            UnSubscribeToEvent(
+                new NetworkEventSubscription(SFSEvent.CONNECTION_LOST, ConnectionLost));
             _smartFox.Disconnect();
         }
 
         private void Start()
         {
-            _smartFox.AddEventListener(SFSEvent.CONNECTION, OnConnect);
-            _smartFox.Connect(_networkModel.ServerIp, _networkModel.ServerPort);
+            SubscribeToEvent(
+                new NetworkEventSubscription(SFSEvent.CONNECTION_LOST, ConnectionLost));
+            InitializeSession();
         }
 
-        // TODO: Handle zone login in a seperate async sequence
-        private void OnConnect(BaseEvent e)
+        public async Task<BaseEvent> SendRequest(IRequest request, string sfsEvent)
         {
-            _smartFox.Send(new LoginRequest("", "", _networkModel.ZoneName));
+            var taskCompletionSource = new TaskCompletionSource<BaseEvent>();
+            if (_smartFox == null)
+            {
+                taskCompletionSource.SetException(
+                    new Exception("Smartfox is not initialized"));
+                return await taskCompletionSource.Task;
+            }
+            // creating a lambda that sets the result
+            // we need to Unsubscribe from it in order to not raise the thing
+            EventListenerDelegate lambda = null;
+            lambda = (BaseEvent e) =>
+            {
+                taskCompletionSource.SetResult(e);
+                _smartFox.RemoveEventListener(sfsEvent, lambda);
+            };
+            _smartFox.AddEventListener(sfsEvent, lambda);
+            _smartFox.Send(request);
+            // What happens when your request times out
+            var timeoutTask = Task.Delay(_networkModel.Timeout).ContinueWith(t =>
+            {
+                taskCompletionSource.TrySetException(new Exception("Request Timed Out"));
+                _smartFox.RemoveEventListener(sfsEvent, lambda);
+            });
+
+            return await Task.WhenAny(timeoutTask, taskCompletionSource.Task) ==
+                           taskCompletionSource.Task
+                       ? await taskCompletionSource.Task
+                       : throw new TimeoutException("Request Timed Out");
+        }
+
+        public void SubscribeToEvent(NetworkEventSubscription subscription) =>
+            _smartFox.AddEventListener(subscription.EventName, subscription.Action);
+
+        public void UnSubscribeToEvent(NetworkEventSubscription subscription) =>
+            _smartFox.RemoveEventListener(subscription.EventName,
+                                          subscription.Action);
+
+        public User GetMyself() => _smartFox.MySelf;
+
+        public Room GetCurrentRoom() => _smartFox.LastJoinedRoom;
+
+        public string GetCurrentZone() => _smartFox.CurrentZone;
+
+        private void ConnectionLost(BaseEvent e) => InitializeSession();
+
+        private async void InitializeSession()
+        {
+            await ConnectToServer();
+            try
+            {
+                await SendRequest(new LoginRequest("", "", _networkModel.ZoneName),
+                                  SFSEvent.LOGIN);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+
+        private async Task<bool> ConnectToServer()
+        {
+            TaskCompletionSource<bool> taskCompletionSource =
+                new TaskCompletionSource<bool>();
+            EventListenerDelegate lambda = null;
+            lambda = evt =>
+            {
+                taskCompletionSource.SetResult(true);
+                _smartFox.RemoveEventListener(SFSEvent.CONNECTION, lambda);
+            };
+            _smartFox.AddEventListener(SFSEvent.CONNECTION, lambda);
+            _smartFox.Connect();
+            var timeoutTask = Task.Delay(_networkModel.Timeout).ContinueWith(t =>
+            {
+                taskCompletionSource.SetException(
+                    new TimeoutException("Request Timed out"));
+                _smartFox.RemoveEventListener(SFSEvent.CONNECTION, lambda);
+            });
+
+            return await Task.WhenAny(timeoutTask, taskCompletionSource.Task) ==
+                           taskCompletionSource.Task
+                       ? await taskCompletionSource.Task
+                       : throw new TimeoutException("Request Timed out");
         }
     }
 }
